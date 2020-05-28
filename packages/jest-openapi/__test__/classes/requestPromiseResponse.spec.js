6e2c19d9495b8b8cea80681faf455ede7ce5ec8b
Luděk Nový <ludek.novy@ceskatelevize.cz>
2020-05-28 22:06:18 +0200

    linter fixes

--- packages/jest-openapi/__test__/classes/requestPromiseResponse.spec.js
+++ packages/jest-openapi/__test__/classes/requestPromiseResponse.spec.js
@@ -4,18 +4,18 @@ const RequestPromiseResponse = require('../../src/openapi-validator/lib/classes/
 describe('RequestPromiseResponse', () => {
   describe('response body as object', () => {
     it('should correctly process body', () => {
-      const body = { test: "object" };
+      const body = { test: 'object' };
       const requestPromiseResponse = new RequestPromiseResponse({ body });
       const processedBody = requestPromiseResponse.getBodyForValidation();
-      expect(processedBody).toEqual(body);
+      expect(processedBody).toStrictEqual(body);
     });
   });
   describe('response body as string', () => {
     it('should correctly process body', () => {
-      const body = `{ "test": "string" }`;
+      const body = '{ "test": "string" }';
       const requestPromiseResponse = new RequestPromiseResponse({ body });
       const processedBody = requestPromiseResponse.getBodyForValidation();
-      expect(processedBody).toEqual({ test: 'string' });
+      expect(processedBody).toStrictEqual({ test: 'string' });
     });
   });
 });
