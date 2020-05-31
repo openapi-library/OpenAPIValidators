c9dfaa7af4345850bd7385dc46dd061ffa5f4cfc
Ludek <novyludek@gmail.com>
2020-05-31 13:03:47 +0200

    Update packages/chai-openapi-response-validator/lib/openapi-validator/lib/classes/RequestPromiseResponse.js
    Co-authored-by: Richard Waller <richard.waller@ibm.com>

--- packages/chai-openapi-response-validator/lib/openapi-validator/lib/classes/RequestPromiseResponse.js
+++ packages/chai-openapi-response-validator/lib/openapi-validator/lib/classes/RequestPromiseResponse.js
@@ -4,7 +4,9 @@ class RequestPromiseResponse extends AbstractResponse {
   constructor(res) {
     super(res);
     this.status = res.statusCode;
-    this.body = res.request.headers.accept === 'application/json' ? res.body : res.body.replace(/"/g, '');
+    this.body = res.request._json // eslint-disable-line no-underscore-dangle
+      ? res.body
+      : res.body.replace(/"/g, '');
     this.req = res.req;
     this.bodyHasNoContent = this.body === '';
   }
