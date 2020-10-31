c35fd47e444319e901663ef3bce142e6c563c7e8
Richard Waller <richard.lh.waller@gmail.com>
2020-10-31 16:49:28 +0000

    docs: fix type definition links to usage examples

--- packages/jest-openapi/index.d.ts
+++ packages/jest-openapi/index.d.ts
@@ -10,12 +10,12 @@ declare global {
     interface Matchers<R> {
       /**
        * Check the HTTP response object satisfies a response defined in your OpenAPI spec.
-       * [See usage example](https://github.com/RuntimeTools/OpenAPIValidators/jest-openapi#in-api-tests-validate-the-status-and-body-of-http-responses-against-your-openapi-spec)
+       * [See usage example](https://github.com/RuntimeTools/OpenAPIValidators/tree/master/packages/jest-openapi#in-api-tests-validate-the-status-and-body-of-http-responses-against-your-openapi-spec)
        */
       toSatisfyApiSpec(): R;
       /**
        * Check the object satisfies a schema defined in your OpenAPI spec.
-       * [See usage example](https://github.com/RuntimeTools/OpenAPIValidators/jest-openapi#in-unit-tests-validate-objects-against-schemas-defined-in-your-openapi-spec)
+       * [See usage example](https://github.com/RuntimeTools/OpenAPIValidators/tree/master/packages/jest-openapi#in-unit-tests-validate-objects-against-schemas-defined-in-your-openapi-spec)
        */
       toSatisfySchemaInApiSpec(schemaName: string): R;
     }
